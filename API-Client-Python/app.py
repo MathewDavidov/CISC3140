5da0561f168a255cb44af635cf1abf0bbb98f829
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2019-09-17 14:38:44 -0400

    add route

--- API-Client-Python/app.py
+++ API-Client-Python/app.py
@@ -7,13 +7,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 url = "https://stats.nba.com/stats/leagueleaders?ActiveFlag=&LeagueID=00&PerMode=Totals&Scope=S&Season=2018-19&SeasonType=Regular+Season&StatCategory=PTS"
-
 obj = urllib.request.urlopen(url)
 read = obj.read()
 data = json.loads(read.decode('utf-8'))
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('index.html', info=data)
 
