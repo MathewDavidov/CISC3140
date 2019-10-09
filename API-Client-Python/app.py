4671e6452057a42fda1bc60db31cb4066caf38d1
katychuang <katychuang@gmail.com>
2019-10-09 16:55:20 -0400

    Switching to table view in UI

--- API-Client-Python/app.py
+++ API-Client-Python/app.py
@@ -6,16 +6,21 @@ from urllib.request import urlopen
 from flask import Flask, render_template
 app = Flask(__name__)
 
-url = "https://stats.nba.com/stats/leagueleaders?ActiveFlag=&LeagueID=00&PerMode=Totals&Scope=S&Season=2018-19&SeasonType=Regular+Season&StatCategory=PTS"
-obj = urllib.request.urlopen(url)
-read = obj.read()
-data = json.loads(read.decode('utf-8'))
+def connect():
+    url = "https://stats.nba.com/stats/leagueleaders?ActiveFlag=&LeagueID=00&PerMode=Totals&Scope=S&Season=2018-19&SeasonType=Regular+Season&StatCategory=PTS"
+    obj = urllib.request.urlopen(url)
+    read = obj.read()
+    data = json.loads(read.decode('utf-8'))
+    return data
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html', info=data)
+    data = connect()
+    h = data['resultSet']['headers']
+    records = data['resultSet']['rowSet']
+    return render_template('index.html', info=records, headers=h)
 
 
 if __name__ == '__main__':
