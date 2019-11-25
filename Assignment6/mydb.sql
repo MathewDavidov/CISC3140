406e6237e250f02ddbc3141c06f7e17f3c22fd05
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2019-11-25 16:26:05 -0500

    update readme to include travis link

---
+++ Assignment6/mydb.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [Feedback] (
+	[Timestamp] VARCHAR(100)  NOT NULL,
+	[Organized] INTEGER  NOT NULL,
+	[SpokeClearly] INTEGER  NOT NULL,
+	[Enthusiastic] INTEGER  NOT NULL,
+	[Learned] INTEGER  NOT NULL,
+	[LearnedWords] VARCHAR(500)  NOT NULL, 
+	[Worked] VARCHAR(500)  NOT NULL,
+	[Changes] VARCHAR(500)  NOT NULL,
+	[Comments] VARCHAR(500)  NULL
+);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CISC3140
+
+[Assignment 8 Travis Link proof](https://travis-ci.com/MathewDavidov/CISC3140/jobs/260001384)
